c544775038c051d2acce9bfb022afad5dfee3011
Mark Lynskey <mlynskey2@gmail.com>
2019-09-07 10:01:32 +1200

    Shows all events

--- day/urls.py
+++ day/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import DayListView
+from .views import DayListView, EventListView
 
 urlpatterns = [
     path('', DayListView.as_view(), name='home'),
+    path('day/<int:pk>/', EventListView.as_view(), name='day'),
 ]
--- day/views.py
+++ day/views.py
@@ -1,9 +1,13 @@
 from django.views.generic import ListView
 
-from .models import Day
+from .models import Day, Event
 
 # Create your views here.
 
 class DayListView(ListView):
     model = Day
     template_name = 'home.html'
+
+class EventListView(ListView):
+    model = Event
+    template_name = 'day.html'
